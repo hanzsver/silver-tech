6b04d8891557ee6460b6e6469ca72e99226cb9a5
hanzsver <hanzsver@gmail.com>
2023-12-22 18:15:13 +0900

    6/seo

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,12 +1,19 @@
-import Container from "components/Container";
+import Container from "../../components/Container";
 import { allPosts } from "contentlayer/generated";
 import { InferGetStaticPropsType } from "next";
 import { useMDXComponent } from "next-contentlayer/hooks";
- 
+
 const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const MDXComponent = useMDXComponent(post.body.code);
+
+  const customMeta = {
+    title: post.title,
+    description: post.description,
+    date: new Date(post.date).toISOString(),
+  };
+
   return (
-    <Container>
+    <Container customMeta={customMeta}>
       <div className="mt-10 prose">
         <h1 className="text-sky-700">{post.title}</h1>
         <MDXComponent />
@@ -14,14 +21,14 @@ const Post = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
     </Container>
   );
 };
- 
+
 export const getStaticPaths = async () => {
   return {
     paths: allPosts.map((p) => ({ params: { slug: p._raw.flattenedPath } })),
     fallback: false,
   };
 };
- 
+
 export const getStaticProps = async ({ params }) => {
   const post = allPosts.find((p) => p._raw.flattenedPath === params.slug);
   return {
@@ -30,5 +37,5 @@ export const getStaticProps = async ({ params }) => {
     },
   };
 };
- 
+
 export default Post;
